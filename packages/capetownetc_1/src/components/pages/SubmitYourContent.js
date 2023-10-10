b6588023451c240c9d5a391c2a03734125417007
Munya Marinda <moyomarinda@gmail.com>
2023-10-10 06:58:13 +0200

    Submit your content page.

--- packages/capetownetc_1/src/components/pages/SubmitYourContent.js
+++ packages/capetownetc_1/src/components/pages/SubmitYourContent.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { connect } from "frontity";
 import Link from "@frontity/components/link";
+import SidebarListingWithImages from "../components/sidebars/SidebarListingWithImages";
 
 const SubmitYourContent = ({
   state,
@@ -8,6 +9,36 @@ const SubmitYourContent = ({
   headerIsSticky,
 }) => {
   const [isSticky, setIsSticky] = useState(false);
+  const postsSet1_categoryID = 7736;
+  const [postsSet1, setPostsSet1] = useState(null);
+  //
+  const WP_SiteUrl = state.source.url;
+  //
+  //
+  useEffect(() => {
+    // FEATURED
+    const fetch1Posts = async () => {
+      try {
+        const response = await fetch(
+          WP_SiteUrl +
+            "/wp-json/wp/v2/posts?categories=" +
+            postsSet1_categoryID +
+            "&per_page=100&orderby=date&order=desc&_embed"
+        );
+        if (!response.ok) {
+          setPostsSet1(false);
+          return;
+        }
+        const postsData = await response.json();
+        setPostsSet1(postsData);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setPostsSet1(false);
+      }
+    };
+    fetch1Posts();
+    //
+  }, []);
   //
   //
   //
@@ -38,19 +69,117 @@ const SubmitYourContent = ({
           ></div>
         </div>
 
-        <div className="post_block_1_parent_2">
-          <div style={{ width: "100%" }}>
-            <div className="subscribe_parent_1">
-              <iframe
-                src="https://hsmcompetitions.co.za/formguru/api.php?form-id=24"
-                width="100%"
-                height="750px"
-                frameborder="0"
-              ></iframe>
+        <div className="post_block_1_parent_2 content_spacing_top_2">
+          <div className="post_block_1_parent_2">
+            <div className="post_block_1_container_1">
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <h1
+                  className="post_block_2_header_1"
+                  style={{
+                    textAlign: "center",
+                  }}
+                >
+                  How You Can Be Featured
+                </h1>
+
+                <p
+                  style={{
+                    width: "70%",
+                    fontSize: "20px",
+                    margin: "40px 0px",
+                    fontWeight: "bold",
+                    textAlign: "center",
+                  }}
+                >
+                  Have you got a story to tell? Are you an aspiring content
+                  creator or influencer? Keen to share your craft?
+                </p>
+
+                <p
+                  style={{
+                    width: "70%",
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    textAlign: "center",
+                  }}
+                >
+                  Cape{"{Town}"}Etc is dedicated to showcasing diverse stories
+                  and perspectives, ranging from news and community projects to
+                  cool places to visit and fun things to do
+                </p>
+
+                <p
+                  style={{
+                    width: "70%",
+                    fontSize: "16px",
+                    margin: "10px 0px 50px 0px",
+                    textAlign: "center",
+                  }}
+                >
+                  We want to share it all through your eyes and offer everyone a
+                  space to feel heard. Drop your content in the box below and
+                  follow the prompts to be featured on our website and/or social
+                  media. You can submit to be credited or remain anonymous.
+                </p>
+
+                <p
+                  style={{
+                    width: "100%",
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Please complete the form below to submit your content
+                </p>
+                <span
+                  style={{
+                    color: "red",
+                    width: "100%",
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  * Required
+                </span>
+
+                <div
+                  style={{
+                    width: "100%",
+                  }}
+                >
+                  <iframe
+                    src="https://hsmcompetitions.co.za/formguru/api.php?form-id=24"
+                    width="100%"
+                    height="750px"
+                    frameborder="0"
+                  ></iframe>
+                </div>
+              </div>
+              <div className="post_block_2_right_bar_parent_1">
+                <div className="ad_square_1">
+                  <p>[AD]</p>
+                </div>
+
+                <SidebarListingWithImages
+                  title="Top Guides"
+                  link={"/things-to-do/"}
+                  postsSet={postsSet1}
+                  WP_SiteUrl={WP_SiteUrl}
+                  postsSet_categoryTitle={"THINGS TO DO"}
+                  postsSet_categoryID={postsSet1_categoryID}
+                />
+              </div>
             </div>
           </div>
         </div>
-
         <div className="ad_vertical_1_parent">
           <div
             className="ad_vertical_1"
